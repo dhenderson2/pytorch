dhenderson2/pytorch

commit a431c068d57bc920581c9fe4f66e51c9f25a8b04

[ci] move sccache stats off RDS (#79366)
Summary:
We want to decommission RDS, so upload sccache stats to S3/GHA as
appropriate.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/79366

Approved by: https://github.com/janeyx99

Test Plan: contbuild & OSS CI, see https://hud.pytorch.org/commit/pytorch/pytorch/3771331b2a692399c228ce0aa2290a9b03010018

Reviewed By: osalpekar

Differential Revision: D37119148

Pulled By: suo

fbshipit-source-id: 0000b03cbc8eabc8acc16da5d38b1764e8e8cab9

.jenkins/pytorch/common_utils.sh
@@ -151,3 +151,12 @@ function checkout_install_torchdynamo() {
   popd
   popd
 }
+
+function print_sccache_stats() {
+  echo 'PyTorch Build Statistics'
+  sccache --show-stats
+
+  sccache --show-stats \
+    | python -m tools.stats.sccache_stats_to_json \
+    > "sccache-stats-${BUILD_ENVIRONMENT}-${OUR_GITHUB_JOB_ID}.json"
+}

.github/workflows/_bazel-build-test.yml
@@ -57,6 +57,13 @@ jobs:
         id: parse-ref
         run: .github/scripts/parse_ref.py
 
+      - name: Get workflow job id
+        id: get-job-id
+        uses: ./.github/actions/get-workflow-job-id
+        if: always()
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+
       - name: Build
         env:
           BUILD_ENVIRONMENT: ${{ inputs.build-environment }}
@@ -69,6 +76,7 @@ jobs:
           PR_LABELS: ${{ toJson(github.event.pull_request.labels.*.name) }}
           TORCH_CUDA_ARCH_LIST: 5.2
           DOCKER_IMAGE: ${{ steps.calculate-docker-image.outputs.docker-image }}
+          OUR_GITHUB_JOB_ID: ${{ steps.get-job-id.outputs.job-id }}
         run: |
           # detached container should get cleaned up by teardown_ec2_linux
           container_name=$(docker run \
@@ -79,6 +87,7 @@ jobs:
             -e PR_LABELS \
             -e SKIP_SCCACHE_INITIALIZATION=1 \
             -e TORCH_CUDA_ARCH_LIST \
+            -e OUR_GITHUB_JOB_ID \
             --env-file="/tmp/github_env_${GITHUB_RUN_ID}" \
             --security-opt seccomp=unconfined \
             --cap-add=SYS_PTRACE \
@@ -143,13 +152,6 @@ jobs:
       - name: Chown workspace
         uses: ./.github/actions/chown-workspace
         if: always()
-
-      - name: Get workflow job id
-        id: get-job-id
-        uses: ./.github/actions/get-workflow-job-id
-        if: always()
-        with:
-          github-token: ${{ secrets.GITHUB_TOKEN }}
 
       - name: Upload test artifacts
         uses: ./.github/actions/upload-test-artifacts

.github/workflows/_linux-build.yml
@@ -74,6 +74,13 @@ jobs:
         id: parse-ref
         run: .github/scripts/parse_ref.py
 
+      - name: Get workflow job id
+        id: get-job-id
+        uses: ./.github/actions/get-workflow-job-id
+        if: always()
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+
       - name: Build
         env:
           BUILD_ENVIRONMENT: ${{ inputs.build-environment }}
@@ -90,6 +97,7 @@ jobs:
           DOCKER_IMAGE: ${{ steps.calculate-docker-image.outputs.docker-image }}
           XLA_CUDA: ${{ contains(inputs.build-environment, 'xla') && '0' || '' }}
           DEBUG: ${{ inputs.build-with-debug && '1' || '0' }}
+          OUR_GITHUB_JOB_ID: ${{ steps.get-job-id.outputs.job-id }}
         run: |
           # detached container should get cleaned up by teardown_ec2_linux
           container_name=$(docker run \
@@ -106,6 +114,7 @@ jobs:
             -e SKIP_SCCACHE_INITIALIZATION=1 \
             -e TORCH_CUDA_ARCH_LIST \
             -e PR_LABELS \
+            -e OUR_GITHUB_JOB_ID \
             --env-file="/tmp/github_env_${GITHUB_RUN_ID}" \
             --security-opt seccomp=unconfined \
             --cap-add=SYS_PTRACE \
@@ -131,6 +140,15 @@ jobs:
           retention-days: 14
           if-no-files-found: error
           path: artifacts.zip
+
+      - name: Upload sccache stats
+        uses: seemethere/upload-artifact-s3@v5
+        with:
+          s3-prefix: |
+            ${{ github.repository }}/${{ github.run_id }}/${{ github.run_attempt }}/artifact
+          retention-days: 365
+          if-no-files-found: error
+          path: sccache-stats-*.json
 
       - name: Teardown Linux
         uses: ./.github/actions/teardown-linux

.github/workflows/_mac-build.yml
@@ -78,7 +78,16 @@ jobs:
           sudo chmod +x /usr/local/bin/sccache
           echo "SCCACHE_BUCKET=ossci-compiler-cache-circleci-v2" >> "${GITHUB_ENV}"
 
+      - name: Get workflow job id
+        id: get-job-id
+        uses: ./.github/actions/get-workflow-job-id
+        if: always()
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+
       - name: Build
+        env:
+          OUR_GITHUB_JOB_ID: ${{ steps.get-job-id.outputs.job-id }}
         run: |
           echo "CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname "$(which conda)")/../"}" >> "${GITHUB_ENV}"
           .jenkins/pytorch/macos-build.sh
@@ -96,3 +105,13 @@ jobs:
           retention-days: 14
           if-no-files-found: error
           path: artifacts.zip
+
+      - name: Upload sccache stats to GHA
+        uses: actions/upload-artifact@v2
+        # Only if sccache is installed, see above
+        if: ${{ github.event_name == 'push' || github.event.pull_request.head.repo.full_name == github.repository }}
+        with:
+          name: sccache-stats-${{ inputs.build-environment }}-runattempt${{ github.run_attempt }}-${{ steps.get-job-id.outputs.job-id }}.json
+          retention-days: 14
+          if-no-files-found: warn
+          path: sccache-stats-*.json

.github/workflows/_win-build.yml
@@ -49,6 +49,13 @@ jobs:
         id: parse-ref
         run: .github/scripts/parse_ref.py
 
+      - name: Get workflow job id
+        id: get-job-id
+        uses: ./.github/actions/get-workflow-job-id
+        if: always()
+        with:
+          github-token: ${{ secrets.GITHUB_TOKEN }}
+
       - name: Build
         shell: bash
         env:
@@ -71,6 +78,7 @@ jobs:
           DEBUG: ${{ inputs.build-with-debug && '1' || '0' }}
           TORCH_CUDA_ARCH_LIST: "7.0"
           USE_CUDA: ${{ inputs.cuda-version != 'cpu' && '1' || '0' }}
+          OUR_GITHUB_JOB_ID: ${{ steps.get-job-id.outputs.job-id }}
         run: |
           .jenkins/pytorch/win-build.sh
 
@@ -82,6 +90,15 @@ jobs:
           if-no-files-found: error
           name: ${{ env.BUILD_ENVIRONMENT }}
           path: C:\${{ github.run_id }}\build-results
+
+      - name: Upload sccache stats
+        uses: seemethere/upload-artifact-s3@v5
+        with:
+          s3-prefix: |
+            ${{ github.repository }}/${{ github.run_id }}/${{ github.run_attempt }}/artifact
+          retention-days: 14
+          if-no-files-found: error
+          path: sccache-stats-*.json
 
       - name: Teardown Windows
         uses: ./.github/actions/teardown-win

.github/workflows/upload-test-stats.yml
@@ -37,6 +37,7 @@ jobs:
         run: |
           echo "${WORKFLOW_URL}"
           python3 tools/stats/upload_test_stats.py --workflow-run-id "${WORKFLOW_RUN_ID}" --workflow-run-attempt "${WORKFLOW_RUN_ATTEMPT}"
+          python3 tools/stats/upload_sccache_stats.py --workflow-run-id "${WORKFLOW_RUN_ID}" --workflow-run-attempt "${WORKFLOW_RUN_ATTEMPT}"
 
   check-api-rate:
     if: ${{ always() }}

.gitignore
@@ -121,6 +121,7 @@ torch/version.py
 env
 .circleci/scripts/COMMIT_MSG
 scripts/release_notes/*.json
+sccache-stats*.json
 
 # These files get copied over on invoking setup.py
 torchgen/packaged/*

.jenkins/pytorch/build-asan.sh
@@ -33,4 +33,6 @@ cd torch-*
 python setup.py build --cmake-only
 popd
 
+print_sccache_stats
+
 assert_git_not_dirty

commit 0f79fe71208395e25c318b3d0f7479eb79934308

do not write sccache stats to json if missing OUR_GITHUB_JOB_ID (#79541)
Allows use of .jenkins/pytorch/build.sh without assuming OUR_GITHUB_JOB_ID is set.  This is a regression caused by #79366.
Pull Request resolved: https://github.com/pytorch/pytorch/pull/79541
Approved by: https://github.com/suo, https://github.com/seemethere

.jenkins/pytorch/common_utils.sh
@@ -156,7 +156,11 @@ function print_sccache_stats() {
   echo 'PyTorch Build Statistics'
   sccache --show-stats
 
-  sccache --show-stats \
-    | python -m tools.stats.sccache_stats_to_json \
-    > "sccache-stats-${BUILD_ENVIRONMENT}-${OUR_GITHUB_JOB_ID}.json"
+  if [[ -n "${OUR_GITHUB_JOB_ID}" ]]; then
+    sccache --show-stats \
+      | python -m tools.stats.sccache_stats_to_json \
+      > "sccache-stats-${BUILD_ENVIRONMENT}-${OUR_GITHUB_JOB_ID}.json"
+  else
+    echo "env var OUR_GITHUB_JOB_ID not set, will not write sccache stats to json"
+  fi
 }