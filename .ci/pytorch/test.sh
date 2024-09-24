dhenderson2/pytorch

commit 03957efa5d89c12e0534cafeed808e78e5f2b11d

[inductor][scheduler] reorder scheduler nodes after fusion to reduce peak memory (#134874)
**Motivations**:
A topological order of the scheduler nodes that optimize the liveness of buffers can reduce the peak memory utilization. This has been observed and studied e.g., [here](https://arxiv.org/pdf/1910.02653) and [here](https://proceedings.mlr.press/v202/steiner23a/steiner23a.pdf).

**Solutions**:
1. implement a peak memory estimator via liveness analysis
2. implement a few memory aware topological sorting algorithms and pick the one with the lowest peak memory

**Results**:
On some models we can reduce the peak memory significantly:
|             model             | batch size | peak_memory baseline | peak_memory new | ratio |
|:-----------------------------:|:----------:|:--------------------:|:---------------:|:-----:|
| alexnet                       | 128        |         1.17         |       0.99      | 1.19  |
| vgg16                         | 64         |         4.10         |       3.57      | 1.15  |
| DebertaV2ForQuestionAnswering | 1          |         11.60        |      10.56      | 1.10  |

In the presence of compiler based AC, peak memory can be further reduced:
|              model             | batch size | peak_memory baseline | peak_memory new | ratio |
|:------------------------------:|:----------:|:--------------------:|:---------------:|:-----:|
| AlbertForMaskedLM              | 4          |         6.87         |       6.43      | 1.07  |
| AlbertForQuestionAnswering     | 4          |         8.69         |       7.76      | 1.12  |
| MobileBertForQuestionAnswering | 128        |         4.67         |       3.90      | 1.20  |

[Here](https://fb.workplace.com/groups/1075192433118967/posts/1499920537312819/?comment_id=1499938843977655&reply_comment_id=1499951630643043) is an internal use case.

**Other infos:**
* neutral model runtime, because the the reordering happens after fusion. So memory saving is _for free_.

.ci/pytorch/test.sh
@@ -1403,7 +1403,7 @@ test_linux_aarch64() {
        inductor/test_pattern_matcher inductor/test_perf inductor/test_profiler inductor/test_select_algorithm inductor/test_smoke \
        inductor/test_split_cat_fx_passes inductor/test_standalone_compile inductor/test_torchinductor \
        inductor/test_torchinductor_codegen_dynamic_shapes inductor/test_torchinductor_dynamic_shapes \
-       --shard "$SHARD_NUMBER" "$NUM_TEST_SHARDS" --verbose
+       --shard "$SHARD_NUMBER" "$NUM_TEST_SHARDS" --verbose inductor/test_memory
 }
 
 if ! [[ "${BUILD_ENVIRONMENT}" == *libtorch* || "${BUILD_ENVIRONMENT}" == *-bazel-* ]]; then

test/inductor/test_memory.py
@@ -1,0 +1,205 @@
+# Owner(s): ["module: inductor"]
+from unittest import mock
+
+import torch
+from torch._C import FileCheck
+from torch._dynamo.utils import same
+from torch._inductor import config, memory
+from torch._inductor.test_case import TestCase
+from torch._inductor.utils import run_and_get_triton_code
+from torch.testing._internal.inductor_utils import HAS_GPU
+
+
+class Foo(torch.nn.Module):
+    """
+    The default compiled graph is
+    graph():
+        ...
+        %op0 : [num_users=2] = call_function[...](args = (%primals_2, %primals_1), ...)
+        %op1 : [num_users=2] = call_function[...](args = (%primals_2, %primals_3), ...)
+        %op2 : [num_users=1] = call_function[...](args = (%op0, %primals_4), ...)
+        %op3 : [num_users=1] = call_function[...](args = (%op1, %primals_5), ...)
+        %op4 : [num_users=1] = call_function[...](args = (%op2,), ...)
+        %op5 : [num_users=1] = call_function[...](args = (%op3,), ...)
+        %op6_op7 : [num_users=1] = call_function[...](args = (%op5, %op4), ...)
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.w1 = torch.nn.Parameter(torch.ones(1, 10))
+        self.w2 = torch.nn.Parameter(torch.ones(1, 1))
+        self.w3 = torch.nn.Parameter(torch.ones(10, 1))
+        self.w4 = torch.nn.Parameter(torch.ones(1, 10))
+
+    def forward(self, x):
+        t1 = torch.matmul(x, self.w1)
+        t2 = torch.matmul(x, self.w2)
+        t3 = torch.matmul(t1, self.w3)
+        t4 = torch.matmul(t2, self.w4)
+        return t3.sum() + t4.sum()
+
+
+class TestOperatorReorderForPeakMemory(TestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.model = Foo().to("cuda")
+        self.inputs = torch.ones((2048, 1), device="cuda")
+        self.orig_reorder_method = memory.reorder_for_peak_memory
+
+    @mock.patch.object(config, "reorder_for_peak_memory", True)
+    def test_reorder_peak_memory(self):
+        outp_corr = self.model(self.inputs)
+        compiled_model = torch.compile(self.model)
+        code = run_and_get_triton_code(compiled_model, self.inputs)
+        (
+            FileCheck()
+            .check("def call(args):")
+            .check("buf1 = ")
+            .check("buf0 = ")
+            .check("buf2 = ")
+            .check("buf4 = ")
+            .check("buf3 = ")
+            .check("buf5 = ")
+            .check("buf7 = ")
+            .run(code)
+        )
+        # check for correctness
+        outp = compiled_model(self.inputs)
+        self.assertTrue(same(outp, outp_corr))
+
+    @mock.patch.object(config, "reorder_for_peak_memory", True)
+    def test_reorder_peak_memory_lpmf(self):
+        outp_corr = self.model(self.inputs)
+
+        def reorder_with_only_lpmf(
+            nodes,
+            name_to_buf,
+            name_to_fused_node,
+            graph_inputs,
+            graph_outputs,
+            methods=None,
+        ):
+            return self.orig_reorder_method(
+                nodes,
+                name_to_buf,
+                name_to_fused_node,
+                graph_inputs,
+                graph_outputs,
+                methods=[memory.topological_sort_lpmf],
+            )
+
+        with mock.patch.object(
+            memory, "reorder_for_peak_memory", reorder_with_only_lpmf
+        ):
+            compiled_model = torch.compile(self.model)
+
+            code = run_and_get_triton_code(compiled_model, self.inputs)
+            (
+                FileCheck()
+                .check("def call(args):")
+                .check("buf1 = ")
+                .check("buf0 = ")
+                .check("buf2 = ")
+                .check("buf4 = ")
+                .check("buf3 = ")
+                .check("buf5 = ")
+                .check("buf7 = ")
+                .run(code)
+            )
+            # check for correctness
+            outp = compiled_model(self.inputs)
+            self.assertTrue(same(outp, outp_corr))
+
+    @mock.patch.object(config, "reorder_for_peak_memory", True)
+    def test_reorder_peak_memory_bfs(self):
+        outp_corr = self.model(self.inputs)
+
+        def reorder_with_only_bfs(
+            nodes,
+            name_to_buf,
+            name_to_fused_node,
+            graph_inputs,
+            graph_outputs,
+            methods=None,
+        ):
+            return self.orig_reorder_method(
+                nodes,
+                name_to_buf,
+                name_to_fused_node,
+                graph_inputs,
+                graph_outputs,
+                methods=[memory.topological_sort_bfs],
+            )
+
+        with mock.patch.object(
+            memory, "reorder_for_peak_memory", reorder_with_only_bfs
+        ):
+            compiled_model = torch.compile(self.model)
+
+            code = run_and_get_triton_code(compiled_model, self.inputs)
+            (
+                FileCheck()
+                .check("def call(args):")
+                .check("buf0 = ")
+                .check("buf2 = ")
+                .check("buf1 = ")
+                .check("buf4 = ")
+                .check("buf3 = ")
+                .check("buf5 = ")
+                .check("buf7 = ")
+                .run(code)
+            )
+            # check for correctness
+            outp = compiled_model(self.inputs)
+            self.assertTrue(same(outp, outp_corr))
+
+    @mock.patch.object(config, "reorder_for_peak_memory", True)
+    def test_reorder_peak_memory_dfs(self):
+        outp_corr = self.model(self.inputs)
+
+        def reorder_with_only_dfs(
+            nodes,
+            name_to_buf,
+            name_to_fused_node,
+            graph_inputs,
+            graph_outputs,
+            methods=None,
+        ):
+            return self.orig_reorder_method(
+                nodes,
+                name_to_buf,
+                name_to_fused_node,
+                graph_inputs,
+                graph_outputs,
+                methods=[memory.topological_sort_dfs],
+            )
+
+        with mock.patch.object(
+            memory, "reorder_for_peak_memory", reorder_with_only_dfs
+        ):
+            compiled_model = torch.compile(self.model)
+
+            code = run_and_get_triton_code(compiled_model, self.inputs)
+            (
+                FileCheck()
+                .check("def call(args):")
+                .check("buf0 = ")
+                .check("buf2 = ")
+                .check("buf4 = ")
+                .check("buf1 = ")
+                .check("buf3 = ")
+                .check("buf5 = ")
+                .check("buf7 = ")
+                .run(code)
+            )
+            # check for correctness
+            outp = compiled_model(self.inputs)
+            self.assertTrue(same(outp, outp_corr))
+
+
+if __name__ == "__main__":
+    from torch._inductor.test_case import run_tests
+
+    if HAS_GPU:
+        run_tests()

torch/_inductor/config.py
@@ -259,6 +259,9 @@ reorder_for_compute_comm_overlap_passes = [
     "raise_comms",
 ]
 
+# enable operator reordering for peak memory optimization
+reorder_for_peak_memory = os.environ.get("TORCHINDUCTOR_REORDER_FOR_PEAK_MEMORY") == "1"
+
 # runtime estimation function for ops
 # for built-in estimation function, pass in "default"; for user-defined estimation function, pass in the function handle
 estimate_op_runtime = "default"

torch/_inductor/scheduler.py
@@ -48,6 +48,7 @@ from .comm_analysis import estimate_nccl_collective_runtime
 from .dependencies import Dep, MemoryDep, StarDep, WeakDep
 from .ir import ComputedBuffer, MultiOutput, MultiOutputLayout
 from .loop_body import LoopBody
+from .memory import MemoryPlanningInfoForBuffer, MemoryPlanningInfoForNode
 from .runtime.runtime_utils import green_text, red_text
 from .sizevars import SimplifyIndexing
 from .utils import (
@@ -77,6 +78,9 @@ class SchedulerBuffer:
     node: ir.Buffer
     defining_op: BaseSchedulerNode
     users: List[NodeUser] = dataclasses.field(default_factory=list)
+    mpi_buffer: MemoryPlanningInfoForBuffer = dataclasses.field(
+        default_factory=MemoryPlanningInfoForBuffer
+    )
 
     def __hash__(self) -> int:
         return hash(self.node.name)
@@ -167,6 +171,7 @@ class BaseSchedulerNode:
     # .min_order = .max_order = X if this node is X-th node in `self.scheduler.nodes`.
     min_order: int
     max_order: int
+    mpi_node: MemoryPlanningInfoForNode
 
     def __init__(self, scheduler: Scheduler) -> None:
         self.scheduler: Scheduler = scheduler
@@ -1804,6 +1809,16 @@ class Scheduler:
         if config._pre_fusion_custom_pass is not None:
             self.nodes = config._pre_fusion_custom_pass(self.nodes)
         self.nodes = self.fuse_nodes(self.nodes)
+        if config.reorder_for_peak_memory:
+            from .memory import reorder_for_peak_memory
+
+            self.nodes = reorder_for_peak_memory(
+                self.nodes,
+                self.name_to_buf,
+                self.name_to_fused_node,
+                set(V.graph.graph_inputs.keys()),
+                set(V.graph.get_output_names()),
+            )
         self.merge_loops()
         self.finalize_multi_template_buffers()
         if config.reorder_for_compute_comm_overlap:

commit 37af7f7149f20d211f8348b2723c025bd7b0719a

fix the inductor tests (#136474)
Fixes https://github.com/pytorch/pytorch/issues/136464 introduced in https://github.com/pytorch/pytorch/pull/134874

Pull Request resolved: https://github.com/pytorch/pytorch/pull/136474
Approved by: https://github.com/malfet

.ci/pytorch/test.sh
@@ -1402,8 +1402,8 @@ test_linux_aarch64() {
        inductor/test_max_autotune inductor/test_memory_planning inductor/test_metrics inductor/test_multi_kernel inductor/test_pad_mm \
        inductor/test_pattern_matcher inductor/test_perf inductor/test_profiler inductor/test_select_algorithm inductor/test_smoke \
        inductor/test_split_cat_fx_passes inductor/test_standalone_compile inductor/test_torchinductor \
-       inductor/test_torchinductor_codegen_dynamic_shapes inductor/test_torchinductor_dynamic_shapes \
-       --shard "$SHARD_NUMBER" "$NUM_TEST_SHARDS" --verbose inductor/test_memory
+       inductor/test_torchinductor_codegen_dynamic_shapes inductor/test_torchinductor_dynamic_shapes inductor/test_memory \
+       --shard "$SHARD_NUMBER" "$NUM_TEST_SHARDS" --verbose
 }
 
 if ! [[ "${BUILD_ENVIRONMENT}" == *libtorch* || "${BUILD_ENVIRONMENT}" == *-bazel-* ]]; then