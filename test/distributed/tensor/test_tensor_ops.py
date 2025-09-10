dhenderson2/pytorch

commit 7b05bdd925f0f4b49e68662f9761fabaa27f2faf

[DTensor] fix copy_ strategy (#158538)
The previous strategy directly used 'self' input strategy for 'src'
input.  The fixed strategy correctly maps the self dim to src dim
so that it works even if the src input is broadcast.

E.g. for this program, broadcasting will occur on dims 0,1,3 of self.

```
self = torch.ones((2,3,4,5))
src = torch.ones((4,1))
self.copy_(src)
```

These are the correct sharding combinations:

|   self   |     src |
|-------|------|
| Shard(0)  |   Replicate() |
| Shard(1)  |   Replicate() |
| Shard(2)  |   Shard(0) |
| Shard(3)  |   Shard(1) |

Pull Request resolved: https://github.com/pytorch/pytorch/pull/158538
Approved by: https://github.com/zpcore, https://github.com/XilunWu, https://github.com/wanchaol
ghstack dependencies: #158495, #158490

torch/distributed/tensor/_ops/_tensor_ops.py
@@ -35,6 +35,8 @@ from torch.distributed.tensor.placement_types import (
     Shard,
 )
 
+from ._pointwise_ops import pointwise_strategy
+
 
 aten = torch.ops.aten
 
@@ -91,46 +93,20 @@ register_op_strategy(
     aten._to_copy.default, schema_info=RuntimeSchemaInfo(static_kwargkey=["dtype"])
 )(propagate_single_input_strategy)
 
-
-@register_op_strategy(aten.copy_.default)
-def copy_strategy(op_schema: OpSchema) -> StrategyType:
-    # TODO: this strategy is incorrect for copy_ in the case that src tensor
-    # is smaller rank than self tensor.  It is possible to select a strategy from self tensor
-    # that is invalid for dst tensor.
-    # It is also problematic to assume that shard(0) on src maps to shard(0) on self, since we
-    # may broadcast a new dim to the left or right of 0 when copying.
-    #
-    # For now, I just keep copy working essentially the way it was before this PR,
-    # but split it out so it can be handled separately in the future.
-    num_tensor_args = 2
-    first_input_strategy = op_schema.args_schema[0]
-    assert isinstance(first_input_strategy, OpStrategy)
-    return OpStrategy(
-        [
-            OpSpec(
-                output_specs=DTensorSpec(
-                    mesh=first_input_strategy.mesh,
-                    placements=strategy.output_spec.placements,
-                    tensor_meta=strategy.output_spec.tensor_meta,
-                ),
-                input_specs=[
-                    DTensorSpec(
-                        mesh=first_input_strategy.mesh,
-                        placements=strategy.output_spec.placements,
-                        tensor_meta=strategy.output_spec.tensor_meta,
-                    )
-                    for _ in range(num_tensor_args)
-                ],
-                redistribute_cost=[
-                    generate_redistribute_costs(
-                        first_input_strategy, strategy.output_spec
-                    )
-                    for _ in range(num_tensor_args)
-                ],
-            )
-            for strategy in first_input_strategy.strategies
-        ]
-    )
+# copy_ is actually a pointwise op with broadcasting, so reuse the pointwise strategy, which takes care of these
+# requirements.
+#
+# Following torch broadcasting semantics (https://docs.pytorch.org/docs/stable/notes/broadcasting.html)
+# - self can not change shape as a result of broadcasting since this is an inplace op
+# - src can broadcast, but when it does it always does so from the trailing end
+# e.g. the last dim of 'src' must match up with the last dim of 'self'
+#
+# DTensor semantics for inplace ops also dictates that we may NOT redistribute our 'self' input.
+# In practice, what this means is
+# - our output strategies should map 1:1 to our 'self' input strategies
+# - our 'src' input may be redistributed to match up with the 'self' input, with the caveat of adjusting for
+#   broadcasting dim
+register_op_strategy(aten.copy_.default)(pointwise_strategy)
 
 
 @register_op_strategy(

test/distributed/tensor/test_tensor_ops.py
@@ -56,10 +56,11 @@ class DistTensorOpsTest(DTensorTestBase):
     @with_comms
     def test_copy_(self):
         device_mesh = DeviceMesh(self.device_type, list(range(self.world_size)))
-        src_specs = [[Replicate()], [Shard(0)]]
-        src_tensor = torch.randn((12, 12))
 
+        # basic test
+        src_tensor = torch.randn((12, 12))
         dst_tensor = torch.zeros(12, 12)
+        src_specs = [[Replicate()], [Shard(0)]]
         dst_specs = [[Replicate()], [Shard(0)]]
         for dst_spec, src_spec in zip(dst_specs, src_specs):
             src_dtensor = distribute_tensor(src_tensor, device_mesh, dst_spec)
@@ -68,22 +69,29 @@ class DistTensorOpsTest(DTensorTestBase):
             dst_tensor.copy_(src_tensor)
             self.assertEqual(dst_dtensor.full_tensor(), dst_tensor)
 
-    # @pytest.mark.xfail
-    # @with_comms
-    # def test_copy_broadcast(self):
-    #     device_mesh = DeviceMesh(self.device_type, list(range(self.world_size)))
-    #     src_specs = [[Replicate()], [Shard(0)]]
-    #     src_tensor = torch.randn((12,))
+        # simple broadcasting
+        src_tensor = torch.randn((128,))
+        dst_tensor = torch.zeros(128, 128)
+        src_specs = [[Replicate()], [Shard(0)]]
+        dst_specs = [[Replicate()], [Shard(1)]]
+        for dst_spec, src_spec in zip(dst_specs, src_specs):
+            src_dtensor = distribute_tensor(src_tensor, device_mesh, src_spec)
+            dst_dtensor = distribute_tensor(dst_tensor, device_mesh, dst_spec)
+            dst_dtensor.copy_(src_dtensor)
+            dst_tensor.copy_(src_tensor)
+            self.assertEqual(dst_dtensor.full_tensor(), dst_tensor)
 
-    #     dst_tensor = torch.zeros(12, 12)
-    #     dst_specs = [[Replicate()], [Shard(1)]]
-    #     for dst_spec, src_spec in zip(dst_specs, src_specs):
-    #         src_dtensor = distribute_tensor(src_tensor, device_mesh, dst_spec)
-    #         dst_dtensor = distribute_tensor(dst_tensor, device_mesh, src_spec)
-    #         # perform a broadcasted copy from Shard(0) to Shard(1) for the worst case
-    #         dst_dtensor.copy_(src_dtensor)
-    #         dst_tensor.copy_(src_tensor)
-    #         self.assertEqual(dst_dtensor.full_tensor(), dst_tensor)
+        # The src specs in this case are designed to not be compatible with the dst_specs, redistribute should happen
+        src_tensor = torch.randn((64, 1))
+        dst_tensor = torch.zeros(16, 32, 64, 128)
+        src_specs = [[Shard(1)], [Shard(1)], [Shard(1)], [Shard(1)]]
+        dst_specs = [[Replicate()], [Shard(0)], [Shard(1)], [Shard(2)]]
+        for dst_spec, src_spec in zip(dst_specs, src_specs):
+            src_dtensor = distribute_tensor(src_tensor, device_mesh, src_spec)
+            dst_dtensor = distribute_tensor(dst_tensor, device_mesh, dst_spec)
+            dst_dtensor.copy_(src_dtensor)
+            dst_tensor.copy_(src_tensor)
+            self.assertEqual(dst_dtensor.full_tensor(), dst_tensor)
 
     @with_comms
     def test_contiguous(self):

commit 14af94757e465870d0b9311bf9b2ae330e92a406

[DTensor] fix copy_ strategy to support linearity (#162460)
Fixing issue introduced in https://github.com/pytorch/pytorch/pull/158538
where `aten.copy_.default` is registered as a pointwise op, but without linearity.

In particular, when both `src` and `dst` tensors have same `Partial` placements, direct copy should happen without redistribute, instead of redistributing both to `Replicate` before making the copy.

This was discovered from silent incorrect results e.g. on `torch.einsum` backward.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/162460
Approved by: https://github.com/zpcore

torch/distributed/tensor/_ops/_pointwise_ops.py
@@ -421,6 +421,7 @@ linear_pointwise_ops = {
     aten.mul_.Scalar: 0,
     aten.mul.Tensor: 2,
     aten.mul_.Tensor: 2,
+    aten.copy_.default: 1,
 }
 
 
@@ -748,9 +749,9 @@ def list_pointwise_strategy(
             args_schema,
             child_strtgy,
             linearity,
-            scalar_tensor_idx=_FUSED_OP_SCALAR_IDX
-            if op_schema.op in fused_ops
-            else None,
+            scalar_tensor_idx=(
+                _FUSED_OP_SCALAR_IDX if op_schema.op in fused_ops else None
+            ),
         )
         list_strategy.append(pointwise_strategy)
     return TupleStrategy(list_strategy)

torch/distributed/tensor/_ops/_tensor_ops.py
@@ -35,8 +35,6 @@ from torch.distributed.tensor.placement_types import (
     Shard,
 )
 
-from ._pointwise_ops import pointwise_strategy
-
 
 aten = torch.ops.aten
 
@@ -92,21 +90,6 @@ register_op_strategy(
 register_op_strategy(
     aten._to_copy.default, schema_info=RuntimeSchemaInfo(static_kwargkey=["dtype"])
 )(propagate_single_input_strategy)
-
-# copy_ is actually a pointwise op with broadcasting, so reuse the pointwise strategy, which takes care of these
-# requirements.
-#
-# Following torch broadcasting semantics (https://docs.pytorch.org/docs/stable/notes/broadcasting.html)
-# - self can not change shape as a result of broadcasting since this is an inplace op
-# - src can broadcast, but when it does it always does so from the trailing end
-# e.g. the last dim of 'src' must match up with the last dim of 'self'
-#
-# DTensor semantics for inplace ops also dictates that we may NOT redistribute our 'self' input.
-# In practice, what this means is
-# - our output strategies should map 1:1 to our 'self' input strategies
-# - our 'src' input may be redistributed to match up with the 'self' input, with the caveat of adjusting for
-#   broadcasting dim
-register_op_strategy(aten.copy_.default)(pointwise_strategy)
 
 
 @register_op_strategy(

test/distributed/tensor/test_tensor_ops.py
@@ -93,6 +93,19 @@ class DistTensorOpsTest(DTensorTestBase):
             dst_tensor.copy_(src_tensor)
             self.assertEqual(dst_dtensor.full_tensor(), dst_tensor)
 
+        # as a pointwise op, need to keep Partial placements without redistribute
+        src_tensor = torch.randn((64, 1))
+        dst_tensor = torch.zeros(16, 32, 64, 128)
+        src_specs = [[Partial()]]
+        dst_specs = [[Partial()]]
+        for dst_spec, src_spec in zip(dst_specs, src_specs):
+            src_dtensor = DTensor.from_local(src_tensor, device_mesh, src_spec)
+            dst_dtensor = DTensor.from_local(dst_tensor, device_mesh, dst_spec)
+            dst_dtensor.copy_(src_dtensor)
+            dst_tensor.copy_(src_tensor)
+            self.assertEqual(dst_dtensor.placements, (Partial(),))
+            self.assertEqual(dst_dtensor._local_tensor, dst_tensor)
+
     @with_comms
     def test_contiguous(self):
         device_mesh = self.build_device_mesh()